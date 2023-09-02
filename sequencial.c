7d8eedac19e0f824470ca3ac56a2d3c6d670dfc4
Jose Maia <maianeto2014@gmail.com>
2023-09-02 12:08:42 -0300

    Multiplicacao de matrizes implementada

--- sequencial.c
+++ sequencial.c
@@ -5,9 +5,8 @@ int main(int argc, char const *argv[]) {
 	int m1, n1, m2, n2;
 	FILE* fptr;
 
-
 	// First matrix
-	fptr = fopen("m1.txt", "r");
+	fptr = fopen(argv[1], "r");
 
 	if(fptr == NULL) {
 		printf("ERROR!\n");
@@ -33,7 +32,7 @@ int main(int argc, char const *argv[]) {
 	fclose(fptr);
 
 	// Second matrix
-	fptr = fopen("m2.txt", "r");
+	fptr = fopen(argv[2], "r");
 
 	if(fptr == NULL) {
 		printf("ERROR!\n");
@@ -58,6 +57,33 @@ int main(int argc, char const *argv[]) {
 
 	fclose(fptr);
 
+	// Matrix resultante
+	int* result;
+	int size3 = m1*n2;
+	result = (int*)malloc(size3 * sizeof(int));
+
+	for(int i = 0; i <= m1; i+=2) {
+		for(int j = 0; j < n2; j++) {
+			int prod1, prod2, sum;
+			prod1 = vector1[i] * vector2[j];
+			prod2 = vector1[i+1] * vector2[j+2];
+			sum = prod1 + prod2;
+			printf("%d\n", sum);
+		}			
+	}	
+
+	/*
+
+	for(int i = 0; i < m1; i++) {
+		for(int j = 0; j < n2; j++) {
+			result[i] = 
+			vector1[i];
+			vector2[i];
+		}
+	}
+
+*/
+/*
 	for(int i = 0; i < size1; i++) {
 		printf("%d\n", vector1[i]);
 	}
@@ -67,6 +93,7 @@ int main(int argc, char const *argv[]) {
 	for(int i = 0; i < size2; i++) {
 		printf("%d\n", vector2[i]);
 	}	
+*/
 
 	return 0;
 }
