ebbffe1958a329d1a82bd2684f1b1f4b79ca2f12
Jose Maia <maianeto2014@gmail.com>
2023-09-02 10:30:33 -0300

    Programa auxiliar ok

---
+++ auxiliar.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[]) {
+	int m1, n1, m2, n2;
+	FILE* fptr;
+
+	m1 = atoi(argv[1]);
+	n1 = atoi(argv[2]);
+	m2 = atoi(argv[3]);
+	n2 = atoi(argv[4]);
+
+
+	// First matrix
+	fptr = fopen("m1.txt", "w");
+
+	if(fptr == NULL) {
+		printf("ERROR!\n");
+		exit(0);
+	}
+
+	// Indicates the dimensions in the file
+	fprintf(fptr, "%d\n", m1);
+	fprintf(fptr, "%d\n", n1);
+
+	// Filling the matrix
+	for(int i = 0; i < (m1*n1); i++) {
+		fprintf(fptr, "%d\n", i);	
+	}
+
+	fclose(fptr);
+
+	// Second matrix
+	fptr = fopen("m2.txt", "w");
+
+	if(fptr == NULL) {
+		printf("ERROR!\n");
+		exit(0);
+	}
+
+	// Indicates the dimensions in the file
+	fprintf(fptr, "%d\n", m2);
+	fprintf(fptr, "%d\n", n2);
+
+	// Filling the matrix
+	for(int i = 0; i < (m2*n2); i++) {
+		fprintf(fptr, "%d\n", i);	
+	}
+
+	fclose(fptr);
+
+	return 0;
+}
